d505895c0da751b47afefcb3cbad05a7acf58d7d
Ayrat <axmetxan@gmail.com>
2024-03-21 15:24:44 +0300

    feat: implement burger menu

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import styles from "./Layout.module.css";
 
 const Layout = () => {
+  const [burgerMenuActive, setBurgerMenuActive] = useState(false);
+
   return (
     <div className={styles.layout}>
       <header className={styles.header}>
@@ -10,7 +12,7 @@ const Layout = () => {
           <h1 className={styles.logoTitle}>Айрат Ахметханов</h1>
           <span className={styles.logoSubTitle}>интерьерный фотограф</span>
         </NavLink>
-        <nav className={styles.nav}>
+        <nav className={`${styles.nav} ${burgerMenuActive ? styles.burgerMenuActive : ""}`}>
           <ul className={styles.navList} id="navList">
             <NavLink to="/" className={styles.navLink}>
               Главная
@@ -28,7 +30,27 @@ const Layout = () => {
               О себе
             </NavLink>
           </ul>
+          <span
+            className={`${styles.closeButton} ${burgerMenuActive ? styles.closeButtonActive : ""}`}
+            onClick={() => {
+              setBurgerMenuActive(false);
+            }}
+            role="presentation"
+          >
+            &times;
+          </span>
         </nav>
+        <div
+          className={`${styles.burgerMenuButton} ${burgerMenuActive ? styles.burgerMenuActive : ""}`}
+          onClick={() => {
+            setBurgerMenuActive(true);
+          }}
+          role="presentation"
+        >
+          <span className={styles.burgerMenuButtonLine} />
+          <span className={styles.burgerMenuButtonLine} />
+          <span className={styles.burgerMenuButtonLine} />
+        </div>
       </header>
       <Outlet />
     </div>
